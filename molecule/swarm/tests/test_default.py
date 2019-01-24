8a9b138e2fc1b88a176e8ceb561c83549a16d62f
till <till@php.net>
2019-01-24 15:44:05 +0100

    Fix: compose file to publish all ports

--- molecule/swarm/tests/test_default.py
+++ molecule/swarm/tests/test_default.py
@@ -14,13 +14,15 @@ def test_traefik_is_running_on_swarm(host):
     assert 'traefik:latest' == out
 
 
-def test_host_based_routing_via_traefik(host):
+def test_example_is_running(host):
     # check that the test http server is running
     out = host.check_output(
         'docker ps --filter "label=test=webserver"'
         + ' --format {%raw%}"{{.Image}}"{%endraw%}')
     assert out == 'deis/example-go'
 
+
+def test_traefik_found_container(host):
     # check that traefik found the test http server
     out = host.check_output(
         'curl -L -k -v -s -H "Host:traefik.traefik-swarm.docker.local"'
